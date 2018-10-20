96ba444d8b2e2f6bf5ce17b60158e6b794479ccd
Javid <javidsoft@yahoo.com>
2018-10-20 03:35:51 +0330

    getting stuff ready

---
+++ src/core/Task.cpp
@@ -0,0 +1,13 @@
+///////////////////////////////////////////////////////
+//	Copyright (C) 2018 Javid Shoaei                  //
+//  Email: javidsoft@yahoo.com                       //
+//                                                   //
+// - Task.cpp                                        //
+//   Describes a task for the download manager       //
+///////////////////////////////////////////////////////
+
+namespace BoostGet {
+	class Task {
+
+	}
+}
